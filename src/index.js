8ab3df552e02d1d5f609f3ea9e7a6f0ac5508ace
Aurélien LEMAITRE <aurelien@disruptual.com>
2018-07-19 17:58:56 +0200

    Create the library

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import VueAvatar from './components/VueAvatar.vue';
 import VueAvatarEditor from './components/VueAvatarEditor.vue';
-export default VueAvatarEditor;
 
-export { VueAvatar, VueAvatarEditor };
+const library = { VueAvatar, VueAvatarEditor };
+
+module.exports = library;
+module.exports.default = library;
